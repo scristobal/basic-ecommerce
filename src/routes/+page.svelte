93deaf5a71f1489903977cd3abe4d2b3e85ec1a4
Samuel Cristobal <el.tobal@gmail.com>
2023-08-14 19:05:10 +0200

    passing PageData as destructured props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 
 <div class="grid h-full grid-cols-10">
 	<div class="col-span-7 bg-white px-16 py-12">
-		<Products cart={data.cart} products={data.products} />
+		<Products {...data} />
 	</div>
 	<div class=" col-span-3 bg-zinc-300 px-9 py-12">
 		<Checkout {...data} />

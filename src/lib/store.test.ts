4465bd313bc3b49dfd9718370c62489c9df29488
Samuel Cristobal <el.tobal@gmail.com>
2023-09-01 16:48:12 +0200

    fix store unit test

--- src/lib/store.test.ts
+++ src/lib/store.test.ts
@@ -1,28 +1,30 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeAll } from 'vitest';
 import { cart } from './store';
 import type { Cart } from './types';
 
 describe('cart', () => {
+    let items: Cart = {};
+
+    beforeAll(() => {
+        cart.subscribe((cart) => (items = cart));
+    });
+
     it('should allow adding items', () => {
         cart.addItemByCode('test');
-
-        let items: Cart = {};
-
-        cart.subscribe((cart) => (items = cart));
 
         expect(items['test']).toBe(1);
     });
 
     it('should allow removing items', () => {
         cart.addItemByCode('test');
-        cart.addItemByCode('test');
-        cart.addItemByCode('test');
         cart.removeItemByCode('test');
 
-        let items: Cart = {};
+        expect(items['test']).toBe(1);
+    });
 
-        cart.subscribe((cart) => (items = cart));
+    it('should remove items', () => {
+        cart.removeItemByCode('test');
 
-        expect(items['test']).toBe(2);
+        expect(items['test']).toBeUndefined();
     });
 });

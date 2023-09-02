8a62a0037648bc94ea9228d462a7f32286b76866
Samuel Cristobal <el.tobal@gmail.com>
2023-09-02 07:36:37 +0200

    fix store tests inter dependencies

--- src/lib/store.test.ts
+++ src/lib/store.test.ts
@@ -1,21 +1,36 @@
-import { describe, it, expect, beforeAll } from 'vitest';
-import { cart } from './store';
+import { describe, it, expect, afterEach, beforeEach } from 'vitest';
+import { createCart } from './store';
 import type { Cart } from './types';
+import type { Unsubscriber } from 'svelte/store';
 
 describe('cart', () => {
     let items: Cart = {};
+    let cart: ReturnType<typeof createCart>;
 
-    beforeAll(() => {
-        cart.subscribe((cart) => (items = cart));
+    let unsubscribe: Unsubscriber;
+
+    beforeEach(() => {
+        cart = createCart();
+        unsubscribe = cart.subscribe((cart) => (items = cart));
+    });
+
+    afterEach(() => {
+        unsubscribe();
+        items = {};
     });
 
     it('should allow adding items', () => {
+        expect(items['test']).toBeUndefined();
+
         cart.addItemByCode('test');
 
         expect(items['test']).toBe(1);
     });
 
     it('should allow removing items', () => {
+        expect(items['test']).toBeUndefined();
+
+        cart.addItemByCode('test');
         cart.addItemByCode('test');
         cart.removeItemByCode('test');
 
@@ -23,6 +38,9 @@ describe('cart', () => {
     });
 
     it('should remove items', () => {
+        expect(items['test']).toBeUndefined();
+
+        cart.removeItemByCode('test');
         cart.removeItemByCode('test');
 
         expect(items['test']).toBeUndefined();
